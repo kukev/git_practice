987a8c5293afa5abbb2b1fe9373bb51ed060f6af
kukev <71849092+kukev@users.noreply.github.com>
2020-11-04 11:42:23 -0600

    Create inurance_function_list_array.py

---
+++ inurance_function_list_array.py
@@ -0,0 +1,34 @@
+# Function to estimate insurance cost:
+def estimate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
+  estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+  print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.")
+  return estimated_cost
+ 
+# Estimate Maria's insurance cost
+maria_insurance_cost = estimate_insurance_cost(name = "Maria", age = 31, sex = 0, bmi = 23.1, num_of_children = 1, smoker = 0)
+
+# Estimate Rohan's insurance cost
+rohan_insurance_cost = estimate_insurance_cost(name = 
+"Rohan", age = 25, sex = 1, bmi = 28.5, num_of_children = 3, smoker = 0)
+
+# Estimate Valentina's insurance cost
+valentina_insurance_cost = estimate_insurance_cost(name = "Valentina", age = 53, sex = 0, bmi = 31.4, num_of_children = 0, smoker = 1)
+
+# Add Estimate of Akira's insurance cost
+akira_insurance_cost = estimate_insurance_cost(name = "Akira", age = 19, sex = 1, bmi = 27.1, num_of_children = 0, smoker = 0)
+
+# Add your code here
+names = ["Maria", "Rohan", "Valentina", "Akira"]
+insurance_costs = [4150.0, 5320.0, 35210.0, 2930]
+insurance_data = list(zip(names, insurance_costs))
+print("Here is the actual insurance cost data:  " + str(insurance_data))
+estimated_insurance_data = []
+estimated_insurance_data.append(("Maria", maria_insurance_cost))
+estimated_insurance_data.append(("Rohan", rohan_insurance_cost))
+estimated_insurance_data.append(("Valentina", valentina_insurance_cost))
+estimated_insurance_data.append(("Akira", akira_insurance_cost))
+print("Here is the estimated insurance cost data:  " + str(estimated_insurance_data))
+for x in range(len(estimated_insurance_data)):
+  name = insurance_data[x][0]
+  print()
+  print("The difference between the estmated and actual insurance costs for " + name + " is "  + str(estimated_insurance_data[x][1]- insurance_data[x][1]) + " dollars.")
